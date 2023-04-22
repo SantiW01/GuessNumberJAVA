57904c31f1c12b7926db791a158ed08b87acb0aa
SantiW01 <sweihmuller01@gmail.com>
2023-04-22 17:12:30 -0300

    First commit

---
+++ src/main/java/com/mycompany/guessnumber/GuessNumber.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guessnumber;
+
+/**
+ *
+ * @author Santiago
+ */
+public class GuessNumber {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
