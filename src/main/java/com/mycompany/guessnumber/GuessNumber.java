399a3e93043ce3a6f02487f5313a65deda932436
SantiW01 <sweihmuller01@gmail.com>
2023-04-26 02:26:54 -0300

    Assign Computer's number as a constant

--- src/main/java/com/mycompany/guessnumber/GuessNumber.java
+++ src/main/java/com/mycompany/guessnumber/GuessNumber.java
@@ -10,7 +10,7 @@ package com.mycompany.guessnumber;
 public class GuessNumber {
     public static void main(String[] args) {
         Game game = new Game();
-        int firstNumber ;
+        final int firstNumber;
         int secondNumber = 0;
         int tries = 0;
         firstNumber = game.ComputerNumber();
